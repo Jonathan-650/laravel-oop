c14e669a5e647c80c8088904448b7be4b0ed0240
Jonathan-650 <jonathanmparhusip@gmail.com>
2026-07-12 19:09:57 +0700

    update menu

--- first-app2/app/Models/Menu.php
+++ first-app2/app/Models/Menu.php
@@ -8,6 +8,8 @@ class Menu extends Model {
             (object)["food" => "Ayam Geprek", "price" => 15],
             (object)["food" => "Fried Rice", "price" => 18],
             (object)["food" => "Matcha", "price" => 10],
+            (object)["food" => "Burger (looks like one? no warranty)", "price" => 20],
+            (object)["food" => "Uranium-235", "price" => 10000],
             (object)["food" => "para-Dimethylaminobenzaldehyde (P-DMAB)", "price" => 20000],
             (object)["food" => "Hydrazine", "price" => 50000]
         ];
